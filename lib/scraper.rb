1b2bfbf023da3120cb7095a289ff8e269399a960
Christy Perozzi <christy.perozzi@gmail.com>
2022-03-10 20:55:58 -0500

    completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+puts doc.css(".heading-financier").text.strip
+
+list_items = doc.css("li")
+
+list_items.each do |li|
+    puts li.text.strip
+end
+
+
